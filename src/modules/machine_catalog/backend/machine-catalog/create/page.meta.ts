003d90adf864a4b511186993f966af6c5e770701
Łukasz Chruściel <lchrusciel@gmail.com>
2026-04-11 18:24:39 +0200

    [MachineCatalog] Fix page edition

--- src/modules/machine_catalog/backend/machine-catalog/create/page.meta.ts
+++ src/modules/machine_catalog/backend/machine-catalog/create/page.meta.ts
@@ -3,6 +3,8 @@ export const metadata = {
   requireFeatures: ['machine_catalog.manage'],
   pageTitle: 'New Machine Profile',
   pageTitleKey: 'machine_catalog.create.title',
+  pageGroup: 'Machines',
+  pageGroupKey: 'machines.nav.group',
   breadcrumb: [
     { label: 'Machine Catalog', labelKey: 'machine_catalog.page.title', href: '/backend/machine-catalog' },
     { label: 'New', labelKey: 'machine_catalog.create.title' },
